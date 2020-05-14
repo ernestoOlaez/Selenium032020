1ab6aae56ca83c5d64e1e11cdcf3db355d365646
olaez <olaez@DESKTOP-LIP6K8G>
2020-05-13 20:43:22 -0500

    Add a class for variables boolean

---
+++ Selenium/BasicJava/DataTypes/BooleanTestExcercise.java
@@ -0,0 +1,21 @@
+package DataTypes;
+
+public class BooleanTestExcercise 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		//Initialize the boolean variable with value true or false, once declaration is done
+		boolean testResult;
+		
+		testResult=true;
+		System.out.println("Test result is="+ testResult);
+		
+		testResult=false;
+		System.out.println("Test result is="+ testResult);
+
+	}
+
+}
