d4bcab3b6f9345d1c63c0c20485b117486ab6fa1
olaez <olaez@DESKTOP-LIP6K8G>
2020-05-23 14:42:45 -0500

    Add class for data Types

---
+++ Selenium/BasicJava/DataTypes/IntegerTestExercise.java
@@ -0,0 +1,24 @@
+package DataTypes;
+
+public class IntegerTestExercise {
+
+	public static void main(String[] args) 
+	{
+
+		//Use the int keyword to declare integer variable
+		int carSpeed;
+		
+		//Initialize a variable with value=20
+		carSpeed=20;
+		
+		//Print the value of integer variable
+		System.out.println("Car is running at the speed of: " + carSpeed);
+		
+		//Change the value of the integer variable
+		carSpeed=carSpeed + 20;
+		
+		//PPrint the value of integer variable
+		System.out.println("Current speed of the car is: " + carSpeed);
+	}
+
+}
